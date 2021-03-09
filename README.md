507006787686c1811b4acbbb3b33eeaf1e443199
sousadelucas <lucasantos1bio@gmail.com>
2021-03-09 16:27:50 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Calculadora de gorjetas
+
+Projeto construído com apoio da one bit code.
+
+Uma calculadora de gorjetas para facilitar a vida de estabelecimentos na hora de cálculos de gorjeta.
+
+Podendo oferecer o valor total da conta com o valor da gorjeta incluído para cada pessoa.
+
+
+
---
+++ scripts.js
@@ -0,0 +1,34 @@
+function calculateTip(event) {
+    event.preventDefault();
+    let bill = document.getElementById('bill').value;
+    let serviceQual = document.getElementById('serviceQual').value;
+    let numOfPeople = document.getElementById('people').value;
+
+    if(bill == '' | serviceQual == 0){
+        alert("Por favor, preencha os valores!")
+        return
+    }
+
+    if(numOfPeople == '' | numOfPeople <=1) { 
+        numOfPeople = 1;
+        document.getElementById('each').style.display = "nome"
+    } else {
+        document.getElementById('each').style.display = "block"
+    }
+
+    let total = (bill * serviceQual) / numOfPeople;
+    total = total.toFixed(2);
+    document.getElementById('tip').innerHTML = total;
+    document .getElementById('totalTip').style.display = "block"
+
+    let totalconta = (bill / numOfPeople) + (bill * serviceQual) / numOfPeople; ;
+    document.getElementById('tiptotal').innerHTML = totalconta;
+    document .getElementById('total').style.display = "block"
+}
+
+document.getElementById('totalTip').style.display = "none"
+document.getElementById('each').style.display = "nome"
+
+document.getElementById('total').style.display = "none"
+
+document.getElementById('tipsForm').addEventListener('submit', calculateTip);
